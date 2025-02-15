b32f28ed1fc7d185c09ba8ff04ce174e4607287b
Aviral Varshney <aviralvarshney07@gmail.com>
2025-02-15 19:01:47 +0530

    Categorised the questions according to Easy, Medium, Hard as per LeetCode, also added Majority Element part 2 in Medium Folder

---
+++ Easy/Majority Element/Solution.java
@@ -0,0 +1,48 @@
+/*
+ 169. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ */
+
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements in the array");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        arr[i] = sc.nextInt();
+        
+        boolean visited[] = new boolean[n];
+
+        for(int i=0;i<n;i++){
+            if(visited[i]==true) continue;
+            int count=1;
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    visited[j]=true;
+                    count++;
+                }
+            }
+            if(count>n/2)
+            System.out.println(arr[i]);
+        }
+        sc.close();
+    }
+}
--- Medium/Majority Element II/Solution.java
+++ Medium/Majority Element II/Solution.java
@@ -1,4 +1,25 @@
-import java.util.Scanner;
+/*
+229. Majority Element II
+
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+ */
+
+import java.util.*;
 
 class Solution {
     public static void main(String args[]) {
@@ -12,6 +33,8 @@ class Solution {
         
         boolean visited[] = new boolean[n];
 
+        ArrayList<Integer> list = new ArrayList<>();
+
         for(int i=0;i<n;i++){
             if(visited[i]==true) continue;
             int count=1;
@@ -21,9 +44,9 @@ class Solution {
                     count++;
                 }
             }
-            if(count>n/2)
-            System.out.println(arr[i]);
+            if(count>n/3) list.add(arr[i]);
         }
+        System.out.println("The elements which are repeated more than n/3 times are: "+list);
         sc.close();
     }
 }
