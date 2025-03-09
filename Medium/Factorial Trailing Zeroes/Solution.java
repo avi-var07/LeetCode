5a8f8e4c1ced08d109ee8831dea15bbb1e7a06f4
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-09 19:06:40 +0530

    Factorial Trailing Zeroes

---
+++ Medium/Factorial Trailing Zeroes/Solution.java
@@ -0,0 +1,46 @@
+/*
+ Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+
+Input: n = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 104
+ */
+import java.util.Scanner;
+class Solution {
+    static int trailingZeroes(int n) {
+        int sum=0;
+        while(n>=5){
+            int d = n/5;
+            sum+=d;
+            n/=5;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println(trailingZeroes(n));
+        sc.close();
+    }
+}
