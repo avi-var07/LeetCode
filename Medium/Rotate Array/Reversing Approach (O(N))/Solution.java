7ff54a576ab26d9d6de3a5d25f62f21ab963fc03
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-12 21:30:28 +0530

    Rotate Array using reversing approach O(N)

---
+++ Medium/Rotate Array/Reversing Approach (O(N))/Solution.java
@@ -0,0 +1,75 @@
+/*Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+ 
+
+Follow up:
+
+Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space? */
+
+import java.util.Scanner;
+
+class Solution {
+    static void reverse(int arr[], int start, int end){
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    static void rotate(int[] nums, int k) {
+        if(k==0) return;
+        int n = nums.length;
+        k=k%n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the number of steps to rotate the array");
+        int k = sc.nextInt();
+        rotate(arr, k);
+        System.out.println("The array after rotating by "+k+" steps is:");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
+// Time Complexity : O(n)
+// Space Complexity : O(1)
