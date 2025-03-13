f934f8d7789bdd494f936357afafe74ea30b9b5b
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-13 13:27:00 +0530

    Check if n and its double exists in O(n^2)

---
+++ Easy/Check If N and Its Double Exist/O(N^2)/Solution.java
@@ -0,0 +1,48 @@
+/*Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+ 
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+ 
+
+Constraints:
+
+2 <= arr.length <= 500
+-103 <= arr[i] <= 103 */
+import java.util.*;
+class Solution {
+    public static boolean checkIfExist(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]*2==arr[j] || arr[j]*2==arr[i]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(checkIfExist(arr));
+        sc.close();
+    }
+}
