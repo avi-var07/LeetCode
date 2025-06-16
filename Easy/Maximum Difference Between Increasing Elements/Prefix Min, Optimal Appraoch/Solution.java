16a575a854c5a115e66a80f540ab096f0bb240bb
Aviral Varshney <aviralvarshney07@gmail.com>
2025-06-16 15:42:07 +0530

    Optimal Prefix Minimum Approach for Maximum Difference Question

---
+++ Easy/Maximum Difference Between Increasing Elements/Prefix Min, Optimal Appraoch/Solution.java
@@ -0,0 +1,62 @@
+/*
+Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+
+Return the maximum difference. If no such i and j exists, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [7,1,5,4]
+Output: 4
+Explanation:
+The maximum difference occurs with i = 1 and j = 2, nums[j] - nums[i] = 5 - 1 = 4.
+Note that with i = 1 and j = 0, the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+Example 2:
+
+Input: nums = [9,4,3,2]
+Output: -1
+Explanation:
+There is no i and j such that i < j and nums[i] < nums[j].
+Example 3:
+
+Input: nums = [1,5,2,10]
+Output: 9
+Explanation:
+The maximum difference occurs with i = 0 and j = 3, nums[j] - nums[i] = 10 - 1 = 9.
+ 
+
+Constraints:
+
+n == nums.length
+2 <= n <= 1000
+1 <= nums[i] <= 109
+ */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        System.out.println("Maximum Difference is: "+maximumDifference(arr));
+        
+        sc.close();
+    }
+    public static int maximumDifference(int[] nums) {
+        int maxDiff = -1, mini=nums[0];
+        for(int i = 1; i<nums.length;i++){
+            if(nums[i]>mini){
+                if(nums[i]-mini>maxDiff)maxDiff=nums[i]-mini;
+            }
+            else mini=nums[i];
+        }
+        return maxDiff;
+    }
+}
