a63c7f21174044ff76a3dca61e0337d96b1dec13
Aviral Varshney <aviralvarshney07@gmail.com>
2025-06-18 14:42:36 +0530

    Buy And Sell Stock Using DP

---
+++ Easy/Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,60 @@
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+ */
+
+
+//DP Approach:
+/*
+If you sell the stock at ith day, then you should buy at minimum price from 1st to i-1th day
+
+ */
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        System.out.println("Maximum Profit is: "+maxProfit(arr));
+        
+        sc.close();
+    }
+    public static int maxProfit(int[] prices) {
+       int profit = 0, mini = prices[0];
+       for(int i=0;i<prices.length;i++){
+        int cost = prices[i]-mini;
+        if(profit<cost)profit=cost;
+        if(mini>prices[i])mini=prices[i];
+       } 
+
+       return profit;
+    }
+}
