5b6d3e9b8472ded267f376842f29f13a3fc04c45
Aviral Varshney <aviralvarshney07@gmail.com>
2025-07-02 15:20:40 +0530

    Set matrix zeroes

---
+++ Medium/Set Matrix Zeroes/Better Brute Force with SC/Solution.java
@@ -0,0 +1,84 @@
+/*Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+ 
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution? */
+
+
+//keep a track on the row & column to be marked as 0 when there is a at least 1 zero in that row or column. then reiterate to mark them zeroes
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int m = sc.nextInt();
+        System.out.println("Enter number of columns: ");
+        int n = sc.nextInt();
+        System.out.println("Enter elements of the matrix:");
+        int arr[][] = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j=0;j<n;j++)
+            arr[i][j] = sc.nextInt();
+        }
+        
+        setZeroes(arr);
+        
+        for(int i = 0; i < m; i++) {
+            for(int j=0;j<n;j++)
+            System.out.print(arr[i][j]+" ");
+
+            System.out.println();
+        }
+
+        
+        sc.close();
+    }
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int toMarkRow[] = new int[m];
+        int toMarkColumn[] = new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    toMarkRow[i]=1;
+                    toMarkColumn[j]=1;
+                }
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(toMarkRow[i]==1||toMarkColumn[j]==1)matrix[i][j]=0;
+            }
+        }
+    }
+}
