5c303ad414e36998950864022f9da4d223682d1f
Aviral Varshney <aviralvarshney07@gmail.com>
2025-07-07 01:47:34 +0530

    Finding Pairs with Certain Sum

---
+++ Medium/Finding Pairs with certain Sum/Solution.java
@@ -0,0 +1,108 @@
+/*You are given two integer arrays nums1 and nums2. You are tasked to implement a data structure that supports queries of two types:
+
+Add a positive integer to an element of a given index in the array nums2.
+Count the number of pairs (i, j) such that nums1[i] + nums2[j] equals a given value (0 <= i < nums1.length and 0 <= j < nums2.length).
+Implement the FindSumPairs class:
+
+FindSumPairs(int[] nums1, int[] nums2) Initializes the FindSumPairs object with two integer arrays nums1 and nums2.
+void add(int index, int val) Adds val to nums2[index], i.e., apply nums2[index] += val.
+int count(int tot) Returns the number of pairs (i, j) such that nums1[i] + nums2[j] == tot.
+ 
+
+Example 1:
+
+Input
+["FindSumPairs", "count", "add", "count", "count", "add", "add", "count"]
+[[[1, 1, 2, 2, 2, 3], [1, 4, 5, 2, 5, 4]], [7], [3, 2], [8], [4], [0, 1], [1, 1], [7]]
+Output
+[null, 8, null, 2, 1, null, null, 11]
+
+Explanation
+FindSumPairs findSumPairs = new FindSumPairs([1, 1, 2, 2, 2, 3], [1, 4, 5, 2, 5, 4]);
+findSumPairs.count(7);  // return 8; pairs (2,2), (3,2), (4,2), (2,4), (3,4), (4,4) make 2 + 5 and pairs (5,1), (5,5) make 3 + 4
+findSumPairs.add(3, 2); // now nums2 = [1,4,5,4,5,4]
+findSumPairs.count(8);  // return 2; pairs (5,2), (5,4) make 3 + 5
+findSumPairs.count(4);  // return 1; pair (5,0) makes 3 + 1
+findSumPairs.add(0, 1); // now nums2 = [2,4,5,4,5,4]
+findSumPairs.add(1, 1); // now nums2 = [2,5,5,4,5,4]
+findSumPairs.count(7);  // return 11; pairs (2,1), (2,2), (2,4), (3,1), (3,2), (3,4), (4,1), (4,2), (4,4) make 2 + 5 and pairs (5,3), (5,5) make 3 + 4
+ 
+
+Constraints:
+
+1 <= nums1.length <= 1000
+1 <= nums2.length <= 105
+1 <= nums1[i] <= 109
+1 <= nums2[i] <= 105
+0 <= index < nums2.length
+1 <= val <= 105
+1 <= tot <= 109
+At most 1000 calls are made to add and count each.
+ */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements of first array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        System.out.println("Enter number of elements of second array: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter " + n1 + " elements:");
+        int arr1[] = new int[n1];
+        for(int i = 0; i < n1; i++) 
+        arr1[i] = sc.nextInt();
+        
+        FindSumPairs obj = new FindSumPairs(arr, arr1);
+
+        System.out.println("Count(5): " + obj.count(5)); // nums1[i] + nums2[j] = 5
+        obj.add(0, 1); // nums2 becomes [4, 4]
+        System.out.println("Count(6): " + obj.count(6));
+        sc.close();
+    }
+}
+class FindSumPairs {
+    private int[] nums1;
+    private int[] nums2;
+    private Map<Integer, Integer> freqMap;
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        freqMap = new HashMap<>();
+
+        for (int num : nums2) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+    }
+
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        int newVal = oldVal + val;
+
+        // update nums2
+        nums2[index] = newVal;
+
+        // update freqMap
+        freqMap.put(oldVal, freqMap.get(oldVal) - 1);
+        if (freqMap.get(oldVal) == 0) {
+            freqMap.remove(oldVal);
+        }
+
+        freqMap.put(newVal, freqMap.getOrDefault(newVal, 0) + 1);
+    }
+
+    public int count(int tot) {
+        int count = 0;
+        for (int num : nums1) {
+            int complement = tot - num;
+            count += freqMap.getOrDefault(complement, 0);
+        }
+        return count;
+    }
+}
