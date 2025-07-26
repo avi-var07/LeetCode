7a87a4a06968fb45fe0f36aa2f5f084abaed166a
Aviral Varshney <aviralvarshney07@gmail.com>
2025-07-26 22:53:12 +0530

    Minimum in rotated sorted array

---
+++ Medium/Find Minimum in rotated sorted array/Solution.java
@@ -0,0 +1,76 @@
+/*Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+All the integers of nums are unique.
+nums is sorted and rotated between 1 and n times.
+ */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        System.out.println("Minimum element in rotated sorted array is: "+findMin(arr));
+        
+        sc.close();
+    }
+    public static int findMin(int[] arr) {
+        int low = 0, high = arr.length-1;
+        int ans = Integer.MAX_VALUE;
+        while(low<=high){
+            if(arr[low]<=arr[high]){
+                if(arr[low]<ans)ans=arr[low];
+                break;
+            }
+            int mid = low+(high-low)/2;
+
+            if(arr[low]<=arr[mid]){
+                if(ans<arr[low])ans=arr[low];
+                low=mid+1;
+            }
+            else{
+                if(arr[mid]<ans)ans=arr[mid];
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+}
