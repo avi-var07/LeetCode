734a54e5e0b8e2516c83292998905d4667ee231e
Aviral Varshney <aviralvarshney07@gmail.com>
2025-08-05 17:42:51 +0530

    Kth Missing Positive Number using Binary Search

--- Easy/Kth Missing Positive Number/Binary Search/Solution.java
+++ Easy/Kth Missing Positive Number/Binary Search/Solution.java
@@ -1,27 +1,28 @@
-/*Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+/*Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Return the kth positive integer that is missing from this array.
 
  
 
 Example 1:
 
-Input: nums = [1,2,3,1], k = 3
-Output: true
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
 Example 2:
 
-Input: nums = [1,0,1,1], k = 1
-Output: true
-Example 3:
-
-Input: nums = [1,2,3,1,2,3], k = 2
-Output: false
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
  
 
 Constraints:
 
-1 <= nums.length <= 105
--109 <= nums[i] <= 109
-0 <= k <= 105
- */
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for 1 <= i < j <= arr.length
+*/
 
 import java.util.*;
 
