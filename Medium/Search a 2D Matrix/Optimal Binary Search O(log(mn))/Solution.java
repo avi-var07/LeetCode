510466ba7f034298a3a514d925028e5a3d03483c
Aviral Varshney <aviralvarshney07@gmail.com>
2025-08-18 16:49:17 +0530

    Search in a 2D Matrix Optimal Binary Search

---
+++ Medium/Search a 2D Matrix/Optimal Binary Search O(log(mn))/Solution.java
@@ -0,0 +1,45 @@
+/*You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+  */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int n = sc.nextInt();
+        System.out.println("Enter number of columns: ");
+        int m = sc.nextInt();
+        System.out.println("Enter " + n*m + " elements:");
+        int arr[][] = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0;j<m;j++)arr[i][j] = sc.nextInt();
+        }
+
+        System.out.println("Enter target to search: ");
+        int target = sc.nextInt();
+
+        System.out.println(searchMatrix(arr, target));
+        sc.close();
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int low = 0, high = (matrix.length*matrix[0].length)-1;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            int row = mid/(matrix[0].length);
+            int col = mid%(matrix[0].length);
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]<target)low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
+}
