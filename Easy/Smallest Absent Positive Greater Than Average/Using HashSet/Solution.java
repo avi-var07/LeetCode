2ff8e2d598faf6f062e6ba16d2c3171c61bdf28d
Aviral Varshney <aviralvarshney07@gmail.com>
2025-09-17 19:01:51 +0530

    Smallest absent positive greater than avg

---
+++ Easy/Smallest Absent Positive Greater Than Average/Using HashSet/Solution.java
@@ -0,0 +1,73 @@
+/*You are given an integer array nums.
+
+Return the smallest absent positive integer in nums such that it is strictly greater than the average of all elements in nums.
+
+The average of an array is defined as the sum of all its elements divided by the number of elements.
+ 
+
+Example 1:
+
+Input: nums = [3,5]
+
+Output: 6
+
+Explanation:
+
+The average of nums is (3 + 5) / 2 = 8 / 2 = 4.
+The smallest absent positive integer greater than 4 is 6.
+Example 2:
+
+Input: nums = [-1,1,2]
+
+Output: 3
+
+Explanation:
+
+​​​​​​​The average of nums is (-1 + 1 + 2) / 3 = 2 / 3 = 0.667.
+The smallest absent positive integer greater than 0.667 is 3.
+Example 3:
+
+Input: nums = [4,-1]
+
+Output: 2
+
+Explanation:
+
+The average of nums is (4 + (-1)) / 2 = 3 / 2 = 1.50.
+The smallest absent positive integer greater than 1.50 is 2.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100 */
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        System.out.println("Smallest absent positive number greater than avg is: "+smallestAbsent(arr));
+        sc.close();
+    }
+
+    public static int smallestAbsent(int[] nums) {
+        int sum = 0;
+        for(int ele: nums)sum+=ele;
+
+        int avg = sum/nums.length+1;
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int ele: nums)set.add(ele);
+
+        while(set.contains(avg)||avg<=0)avg++;
+
+        return avg;
+    }
+}
