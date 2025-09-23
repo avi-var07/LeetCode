bbdf10d224bab965f6a5e89e919f95069e3c5d83
Aviral Varshney <aviralvarshney07@gmail.com>
2025-09-24 00:05:21 +0530

    Sort characters by frequency

---
+++ Medium/Sort Characters by Frequency/Solution.java
@@ -0,0 +1,83 @@
+/*Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 105
+s consists of uppercase and lowercase English letters and digits. */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = sc.nextLine();
+
+        Solution sol = new Solution();
+
+        
+        System.out.println("Sorted String based on their high freq to low: "+sol.frequencySort(str));
+        sc.close();
+    }
+
+    class Sorting implements Comparable<Sorting>{
+        char ch;
+        int freq;
+
+        Sorting(char ch, int freq){
+            this.freq=freq;
+            this.ch = ch;
+
+        }
+
+        public int compareTo(Sorting obj){
+            return obj.freq-this.freq;
+        }
+    }
+    
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch: s.toCharArray())map.put(ch,map.getOrDefault(ch, 0)+1);
+
+        PriorityQueue<Sorting> pq = new PriorityQueue<>();
+
+        for(var ele: map.entrySet()){
+            Sorting obj = new Sorting(ele.getKey(), ele.getValue());
+            pq.offer(obj);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(pq.size()>0){
+            Sorting obj = pq.poll();
+            while(obj.freq>0){
+                sb.append(obj.ch);
+                obj.freq--;
+            }
+        }
+        return sb.toString();
+    }
+}
