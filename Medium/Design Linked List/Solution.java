80585ea3417a313983fd6dd853320517f1afff10
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-09 14:03:42 +0530

    Design Linked List

---
+++ Medium/Design Linked List/Solution.java
@@ -0,0 +1,175 @@
+/*Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement the MyLinkedList class:
+
+MyLinkedList() Initializes the MyLinkedList object.
+int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+void addAtTail(int val) Append a node of value val as the last element of the linked list.
+void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+ 
+
+Example 1:
+
+Input
+["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+[[], [1], [3], [1, 2], [1], [1], [1]]
+Output
+[null, null, null, null, 2, null, 3]
+
+Explanation
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.get(1);              // return 2
+myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.get(1);              // return 3
+ 
+
+Constraints:
+
+0 <= index, val <= 1000
+Please do not use the built-in LinkedList library.
+At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and deleteAtIndex.
+ 
+ */
+
+class Node{
+    Node next;
+    Node back;
+    int data;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+        this.back=null;
+    }
+}
+
+class MyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+    public MyLinkedList() {
+        head=null;
+        tail=null;
+        size=0;
+    }
+    
+    public int get(int index) {
+        Node temp = head;
+        int count=0;
+        while(temp!=null){
+            if(count==index)return temp.data;
+            temp=temp.next;
+            count++;
+        }
+        return -1;
+    }
+    
+    public void addAtHead(int val) {
+        Node temp = new Node(val);
+        if(head==null){
+            head=temp;
+            tail=temp;
+            
+        }
+        else{
+            temp.next=head;
+            head.back = temp;
+            head=temp;
+        }
+
+        size++;
+
+    }
+    
+    public void addAtTail(int val) {
+        Node temp = new Node(val);
+        if(tail==null){
+            head=temp;
+            tail=temp;
+
+        }
+        else{
+            tail.next=temp;
+            temp.back= tail;
+            tail=temp;
+        }
+        size++;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index<0||index>size)return;
+
+        if(index==0){
+            addAtHead(val);
+            return;
+        }
+
+        if(index==size){
+            addAtTail(val);
+            return;
+        }
+
+        Node temp = head;
+        int count=0;
+        while(temp!=null){
+            if(count==index)break;
+            temp=temp.next;
+            count++;
+        }
+        Node prev = temp.back;
+        Node newNode = new Node(val);
+        newNode.next=temp;
+        newNode.back = prev;
+        prev.next=newNode;
+        temp.back=newNode;
+
+        size++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index<0||index>=size)return;
+
+        if(index==0){
+            head=head.next;
+            if(head!=null)head.back=null;
+            else tail=null;
+
+        }
+        else if(index==size-1){
+            tail=tail.back;
+            tail.next=null;
+        }
+        else{
+            Node temp = head;
+            int count=0;
+            while(count<index){
+            
+                temp=temp.next;
+                count++;
+            }
+            Node prev = temp.back;
+            Node next = temp.next;
+
+            prev.next = next;
+            next.back = prev;
+        }
+        size--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
