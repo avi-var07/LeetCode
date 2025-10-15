4662a2e9d5ccdd4697e7124f58c9a21c6d3b9891
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-15 23:53:21 +0530

    Remove elements from Linked List

---
+++ Easy/Remove Linked List Elements/Solution.java
@@ -0,0 +1,85 @@
+/*Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50 */
+import java.util.Scanner;
+
+class ListNode {
+    ListNode next;
+    int val;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter length of the LinkedList: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter " + n + " elements of the LinkedList: ");
+        int val = sc.nextInt();
+        ListNode head = new ListNode(val);
+        ListNode current = head;
+        for (int i = 1; i < n; i++) {
+            int ele = sc.nextInt();
+            ListNode newNode = new ListNode(ele);
+            newNode.next = null;
+            current.next = newNode;
+            current = newNode;
+        }
+        print(head);
+        System.out.println();
+        System.out.println("Enter element to remove: ");
+        int k=sc.nextInt();
+        head=removeElements(head, k);
+        print(head);
+        sc.close();
+    }
+    static void print(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + "  ");
+            temp = temp.next;
+        }
+    }
+    public static ListNode removeElements(ListNode head, int val) {
+        if(head==null)return head;
+        if(head.next==null&&head.val==val)return null;
+        while(head!=null&&head.val==val)head=head.next;
+
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp!=null){
+            if(temp.val==val)prev.next=temp.next;
+            else prev=temp;
+            temp=temp.next;
+        }
+        return head;
+    }
+}
