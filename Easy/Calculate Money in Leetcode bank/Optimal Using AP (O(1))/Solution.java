f3f6ce4be641c22999d3bc8622a2c6807dabd14d
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-26 20:12:09 +0530

    Calculate Money in Leetcode bank

---
+++ Easy/Calculate Money in Leetcode bank/Optimal Using AP (O(1))/Solution.java
@@ -0,0 +1,55 @@
+/*Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+
+He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+
+Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 10
+Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+Example 2:
+
+Input: n = 10
+Output: 37
+Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+Example 3:
+
+Input: n = 20
+Output: 96
+Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+ 
+
+Constraints:
+
+1 <= n <= 1000 */
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Total Money: "+totalMoney(n));
+
+        sc.close();
+    }
+    public static int totalMoney(int n) {
+        int completedWeeks = n/7;
+        int remaining = n%7;
+        int first = 28;
+        int last = 28+(completedWeeks-1)*7;
+        int sum = completedWeeks*(first+last)/2;
+        int start_money = completedWeeks+1;
+        for(int i=1;i<=remaining;i++){
+            sum+=start_money;
+            start_money++;
+        }
+        return sum;
+    }
+}
