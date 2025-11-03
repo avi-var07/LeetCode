7b5eb26b9d228904d826cd457ab009a4698f1d9c
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-03 11:18:45 +0530

    Minimum time to make rope colorful

---
+++ Medium/Minimum Time to Make Rope Colorful/Solution.java
@@ -0,0 +1,73 @@
+/*Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+
+Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+
+Return the minimum time Bob needs to make the rope colorful.
+
+ 
+
+Example 1:
+
+
+Input: colors = "abaac", neededTime = [1,2,3,4,5]
+Output: 3
+Explanation: In the above image, 'a' is blue, 'b' is red, and 'c' is green.
+Bob can remove the blue balloon at index 2. This takes 3 seconds.
+There are no longer two consecutive balloons of the same color. Total time = 3.
+Example 2:
+
+
+Input: colors = "abc", neededTime = [1,2,3]
+Output: 0
+Explanation: The rope is already colorful. Bob does not need to remove any balloons from the rope.
+Example 3:
+
+
+Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+Output: 2
+Explanation: Bob will remove the balloons at indices 0 and 4. Each balloons takes 1 second to remove.
+There are no longer two consecutive balloons of the same color. Total time = 1 + 1 = 2.
+ 
+
+Constraints:
+
+n == colors.length == neededTime.length
+1 <= n <= 105
+1 <= neededTime[i] <= 104
+colors contains only lowercase English letters.
+ 
+ */
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println("Enter String: ");
+        String colors = sc.nextLine();
+        System.out.println("Minimum cost is: "+minCost(colors, arr));
+        
+        sc.close();
+    }
+    public static int minCost(String colors, int[] neededTime) {
+        int n = neededTime.length;
+        int ans = 0;
+        int i = 1;
+        while(i<n){
+            if(colors.charAt(i)==colors.charAt(i-1)){
+                ans+=Math.min(neededTime[i], neededTime[i-1]);
+                neededTime[i]=Math.max(neededTime[i], neededTime[i-1]);
+            }
+            i++;
+            
+        }
+        return ans;
+    }
+}
