26d37a650f95dc3b1d24a27e1e1706a3f21fb916
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-10 22:47:26 +0530

    Sum of subarray ranges

---
+++ Medium/Sum of Subarray Ranges/Brute Force/Solution.java
@@ -0,0 +1,78 @@
+/*You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
+
+Return the sum of all subarray ranges of nums.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 4
+Explanation: The 6 subarrays of nums are the following:
+[1], range = largest - smallest = 1 - 1 = 0 
+[2], range = 2 - 2 = 0
+[3], range = 3 - 3 = 0
+[1,2], range = 2 - 1 = 1
+[2,3], range = 3 - 2 = 1
+[1,2,3], range = 3 - 1 = 2
+So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+Example 2:
+
+Input: nums = [1,3,3]
+Output: 4
+Explanation: The 6 subarrays of nums are the following:
+[1], range = largest - smallest = 1 - 1 = 0
+[3], range = 3 - 3 = 0
+[3], range = 3 - 3 = 0
+[1,3], range = 3 - 1 = 2
+[3,3], range = 3 - 3 = 0
+[1,3,3], range = 3 - 1 = 2
+So the sum of all ranges is 0 + 0 + 0 + 2 + 0 + 2 = 4.
+Example 3:
+
+Input: nums = [4,-2,-3,4,1]
+Output: 59
+Explanation: The sum of all subarray ranges of nums is 59.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you find a solution with O(n) time complexity?
+
+  */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println(subArrayRanges(arr));
+        sc.close();
+    }
+    public static long subArrayRanges(int[] nums) {
+        int n = nums.length;
+      
+        long ans = 0;
+        for(int i = 0;i<n;i++){
+            int largest = nums[i], smallest = nums[i];
+            for(int j=i+1;j<n;j++){
+                if(nums[j]>largest)largest=nums[j];
+                if(nums[j]<smallest)smallest = nums[j];
+                ans+=(largest-smallest);
+            }
+        }
+        return ans;
+    }
+}
