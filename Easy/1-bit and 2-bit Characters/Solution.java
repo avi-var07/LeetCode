21bff408c8e57bc49f319160c7a256b63421c0ce
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-18 10:19:46 +0530

    1-bit and 2-bit Characters

---
+++ Easy/1-bit and 2-bit Characters/Solution.java
@@ -0,0 +1,52 @@
+/*We have two special characters:
+
+The first character can be represented by one bit 0.
+The second character can be represented by two bits (10 or 11).
+Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+
+ 
+
+Example 1:
+
+Input: bits = [1,0,0]
+Output: true
+Explanation: The only way to decode it is two-bit character and one-bit character.
+So the last character is one-bit character.
+Example 2:
+
+Input: bits = [1,1,1,0]
+Output: false
+Explanation: The only way to decode it is two-bit character and two-bit character.
+So the last character is not one-bit character.
+ 
+
+Constraints:
+
+1 <= bits.length <= 1000
+bits[i] is either 0 or 1. */
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println(isOneBitCharacter(arr));
+        sc.close();
+    }
+    public static boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length;
+        int i = 0;
+        while(i<n-1){
+            if(bits[i]==0)i++;
+            else i+=2;
+        }
+        return i==n-1;
+    }
+}
