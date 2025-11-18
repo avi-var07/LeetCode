33911562cb8388e86f52c1a41936a4499cc111ce
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-18 11:14:14 +0530

    Optimal Celebrity Problem Solution

---
+++ Medium/The Celebrity Problem/Brute Force/Solution.java
@@ -0,0 +1,62 @@
+/*A celebrity is a person who is known to all but does not know anyone at a party. A party is being organized by some people. A square matrix mat[][] of size n*n is used to represent people at the party such that if an element of row i and column j is set to 1 it means ith person knows jth person. You need to return the index of the celebrity in the party, if the celebrity does not exist, return -1.
+
+Note: Follow 0-based indexing.
+
+Examples:
+
+Input: mat[][] = [[1, 1, 0],
+                [0, 1, 0],
+                [0, 1, 1]]
+Output: 1
+Explanation: 0th and 2nd person both know 1st person and 1st person does not know anyone. Therefore, 1 is the celebrity person.
+Input: mat[][] = [[1, 1], 
+                [1, 1]]
+Output: -1
+Explanation: Since both the people at the party know each other. Hence none of them is a celebrity person.
+Input: mat[][] = [[1]]
+Output: 0
+Constraints:
+1 ≤ mat.size() ≤ 1000
+0 ≤ mat[i][j] ≤ 1
+mat[i][i] = 1 */
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter matrix length: ");
+        int n = sc.nextInt();
+
+        int arr[][] = new int[n][n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++)arr[i][j] = sc.nextInt();
+        }
+        System.out.println(celebrity(arr));
+        sc.close();
+    }
+    public static int celebrity(int mat[][]) {
+        // code here
+        int n = mat.length;
+        int knowMe[] = new int[n];
+        int iKnow[] = new int[n];
+        
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(mat[i][j]==1){
+                    iKnow[i]++;
+                    knowMe[j]++;
+                }
+            }
+        }
+        
+        int i=0;
+        while(i<n){
+            if(knowMe[i]==n-1&&iKnow[i]==0)return i;
+            i++;
+        }
+        return -1;
+    }
+}
