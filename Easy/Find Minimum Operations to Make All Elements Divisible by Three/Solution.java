40c1ce8c11899befb66c7ed4a12f06b0e9f3b766
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-22 22:31:49 +0530

    Find Minimum Operations to Make All Elements Divisible by Three

---
+++ Easy/Find Minimum Operations to Make All Elements Divisible by Three/Solution.java
@@ -0,0 +1,51 @@
+/*You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+
+Return the minimum number of operations to make all elements of nums divisible by 3.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+
+Output: 3
+
+Explanation:
+
+All array elements can be made divisible by 3 using 3 operations:
+
+Subtract 1 from 1.
+Add 1 to 2.
+Subtract 1 from 4.
+Example 2:
+
+Input: nums = [3,6,9]
+
+Output: 0
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50 */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        sc.close();
+    }
+    public static int minimumOperations(int[] nums) {
+        int ans = 0;
+        for(int ele: nums)if(ele%3!=0)ans++;
+        return ans;
+    }
+}
