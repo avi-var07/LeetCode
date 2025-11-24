776db229e7b4188ed1a5ac624b4ed389c33d91d4
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-24 14:44:44 +0530

    Binary Prefix Divisible By 5

---
+++ Easy/Binary Prefix Divisible By 5/Solution.java
@@ -0,0 +1,54 @@
+/*You are given a binary array nums (0-indexed).
+
+We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).
+
+For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
+Return an array of booleans answer where answer[i] is true if xi is divisible by 5.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,1]
+Output: [true,false,false]
+Explanation: The input numbers in binary are 0, 01, 011; which are 0, 1, and 3 in base-10.
+Only the first number is divisible by 5, so answer[0] is true.
+Example 2:
+
+Input: nums = [1,1,1]
+Output: [false,false,false]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1. */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        List<Boolean>ans = prefixesDivBy5(arr);
+        for(var ele: ans)System.out.print(ele+" ");
+        
+        sc.close();
+    }
+    public static List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean>ans = new ArrayList<>();
+        
+        int curr = 0, n = nums.length;
+        for(int i=0;i<n;i++){
+            curr = (curr*2+nums[i])%5;
+            if(curr==0)ans.add(true);
+            else ans.add(false);
+        }
+        return ans;
+    }
+}
