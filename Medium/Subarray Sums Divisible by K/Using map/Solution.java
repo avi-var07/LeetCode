5ef89f5721dcdddedea7f1cc14948dfc61ae7b92
Aviral Varshney <aviralvarshney07@gmail.com>
2025-12-01 22:55:41 +0530

    Subarray sums divisible by k using mao

---
+++ Medium/Subarray Sums Divisible by K/Using map/Solution.java
@@ -0,0 +1,55 @@
+/*Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+2 <= k <= 104 */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println("Enter k: ");
+        int k = sc.nextInt();
+        System.out.println(subarraysDivByK(arr, k));
+        sc.close();
+    }
+    public static int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer>map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0,ans=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            int rem = sum%k;
+            if(rem<0)rem+=k;
+            if(map.containsKey(rem))ans+=map.get(rem);
+            map.put(rem, map.getOrDefault(rem,0)+1);
+        }
+        return ans;
+    }
+}
