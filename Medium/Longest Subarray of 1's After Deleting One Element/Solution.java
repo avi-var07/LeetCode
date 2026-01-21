4269f67b0f7d74d46b8f99bb6cb58f06a2b404dd
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-21 13:26:41 +0530

    Longest Subarray of 1's after deleting one element

---
+++ Medium/Longest Subarray of 1's After Deleting One Element/Solution.java
@@ -0,0 +1,61 @@
+/*Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1. */
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println(longestSubarray(arr));
+        sc.close();
+    }
+    public static int longestSubarray(int[] arr) {
+        int n = arr.length;
+        int left = 0, right =0;
+        int ans = -1;
+        int count =0;
+        while(right<n){
+            if(arr[right]==0)count++;
+            while(count>1){
+                if(arr[left]==0)count--;
+                left++;
+            }
+            int len = right-left+1;
+            ans = Math.max(len,ans);
+            right++;
+        }
+        return ans-1;
+    }
+}
