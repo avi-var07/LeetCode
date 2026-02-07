8f86cc8d7621e02b72a2fe730a61e25be4d75cb7
Aviral Varshney <aviralvarshney07@gmail.com>
2026-02-07 23:23:47 +0530

    Level Order

---
+++ Medium/Binary Tree Level Order Traversal/Solution.java
@@ -0,0 +1,80 @@
+/*Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000 */
+
+import java.util.*;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val=val;
+    }
+}
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter node (-1: stop): ");
+        
+        TreeNode root = buildTree(sc);
+
+        List<List<Integer>>ans = levelOrder(root);
+
+        for(var ele: ans)System.out.print(ele+" ");
+        
+        sc.close();
+    }
+    static TreeNode buildTree(Scanner sc){
+
+        int val = sc.nextInt();
+        if(val==-1)return null;
+
+        TreeNode root = new TreeNode(val);
+        root.left=buildTree(sc);
+        root.right=buildTree(sc);
+
+        return root;
+    }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>>ans = new ArrayList<>();
+        if(root==null)return ans;
+        Queue<TreeNode>queue = new LinkedList<>();
+
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer>level = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                if(node.left!=null)queue.add(node.left);
+                if(node.right!=null)queue.add(node.right);
+
+                level.add(node.val);
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+
+}
