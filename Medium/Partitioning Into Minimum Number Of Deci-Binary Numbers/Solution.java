85b1d5f92feb9db7097482f4f765b462fddf2cad
Aviral Varshney <aviralvarshney07@gmail.com>
2026-03-01 15:47:20 +0530

    Wrote approach

--- Medium/Partitioning Into Minimum Number Of Deci-Binary Numbers/Solution.java
+++ Medium/Partitioning Into Minimum Number Of Deci-Binary Numbers/Solution.java
@@ -36,7 +36,23 @@ class Solution{
         sc.close();
     }
     public static int minPartitions(String s) {
+        //82734 hai agr to possible deci binary:
+        //00000, 00001, 00011, 00111,01111 nhi ho skta kyuki leading zeroes not allowed
+        //to mtlb humlog length se bada deci binary jaa nhi skte
+        //possible ky ky hai 11111, 10001,10111 etc... ab humesha bada inmei se chunenge aur usse minus krenge taaki minimum operations mei kaam hojaye:
+        //82734-11111 = 71623
+        //71623-11111 = 60512
+        //60512-11111=50401 (ismei carry wala nhi jayega 60512-11111 carry wgerah krk 49401 bn jata )
+        //50401-11111 = 40300
+        //40300-11111=30200
+        //30200-11111=20100
+        //20100-11111=10000
+        //10000-10000=00000
 
+        //total=8....
+
+
+        //trick ye hai ki we can observe ki har iteration pe 1 reduce horha hai, to maximum digit k barabar operations lgenge
         int maxi=-1;
         for(char ch: s.toCharArray())if(ch-'0'>maxi)maxi=ch-'0';
 
