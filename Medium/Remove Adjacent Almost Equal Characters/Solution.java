e6972068b6a6da521c2eef4eeabdeee7ea70f2bc
Aviral Varshney <aviralvarshney07@gmail.com>
2026-03-06 00:09:03 +0530

    Remove Adjacent Almost Equal Character

---
+++ Medium/Remove Adjacent Almost Equal Characters/Solution.java
@@ -0,0 +1,56 @@
+/*You are given a 0-indexed string word.
+
+In one operation, you can pick any index i of word and change word[i] to any lowercase English letter.
+
+Return the minimum number of operations needed to remove all adjacent almost-equal characters from word.
+
+Two characters a and b are almost-equal if a == b or a and b are adjacent in the alphabet.
+
+ 
+
+Example 1:
+
+Input: word = "aaaaa"
+Output: 2
+Explanation: We can change word into "acaca" which does not have any adjacent almost-equal characters.
+It can be shown that the minimum number of operations needed to remove all adjacent almost-equal characters from word is 2.
+Example 2:
+
+Input: word = "abddez"
+Output: 2
+Explanation: We can change word into "ybdoez" which does not have any adjacent almost-equal characters.
+It can be shown that the minimum number of operations needed to remove all adjacent almost-equal characters from word is 2.
+Example 3:
+
+Input: word = "zyxyxyz"
+Output: 3
+Explanation: We can change word into "zaxaxaz" which does not have any adjacent almost-equal characters. 
+It can be shown that the minimum number of operations needed to remove all adjacent almost-equal characters from word is 3.
+ 
+
+Constraints:
+
+1 <= word.length <= 100
+word consists only of lowercase English letters. */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter s: ");
+        String word= sc.nextLine();
+        System.out.println(removeAlmostEqualCharacters(word));
+        sc.close(); 
+    }
+    public static int removeAlmostEqualCharacters(String word) {
+        int ans=0;
+        int n=word.length();
+        for(int i=1;i<n;i++){
+            if(Math.abs(word.charAt(i)-word.charAt(i-1))<=1){
+                ans++;
+                i++;
+            }
+        }
+        return ans;
+    }
+}
