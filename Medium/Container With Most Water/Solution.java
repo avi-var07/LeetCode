a5b1559fdff484343d6e3c4ce75d35c9a2739692
Aviral Varshney <aviralvarshney07@gmail.com>
2026-03-09 23:56:44 +0530

    Container With Most Water

---
+++ Medium/Container With Most Water/Solution.java
@@ -0,0 +1,58 @@
+/*You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104 */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println(maxArea(arr));
+        sc.close();
+    }
+    public static int maxArea(int[] arr) {
+        int n =arr.length;
+        int l=0, r=n-1;
+        int ans=0;
+        while(l<r){
+            int height = Math.min(arr[l], arr[r]);
+            int width = r-l;
+            int area = height*width;
+            ans=Math.max(ans, area);
+
+            if(arr[l]<arr[r])l++;
+            else r--;
+        }
+        return ans;
+    }
+}
