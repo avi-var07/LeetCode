9ca9d091ed5f4c1ca500c38b65f72447b799c593
Aviral Varshney <aviralvarshney07@gmail.com>
2026-03-12 00:02:04 +0530

    Sort the Students by Their Kth Score

---
+++ Medium/Sort the Students by Their Kth Score/Solution.java
@@ -0,0 +1,67 @@
+/*There is a class with m students and n exams. You are given a 0-indexed m x n integer matrix score, where each row represents one student and score[i][j] denotes the score the ith student got in the jth exam. The matrix score contains distinct integers only.
+
+You are also given an integer k. Sort the students (i.e., the rows of the matrix) by their scores in the kth (0-indexed) exam from the highest to the lowest.
+
+Return the matrix after sorting it.
+
+ 
+
+Example 1:
+
+
+Input: score = [[10,6,9,1],[7,5,11,2],[4,8,3,15]], k = 2
+Output: [[7,5,11,2],[10,6,9,1],[4,8,3,15]]
+Explanation: In the above diagram, S denotes the student, while E denotes the exam.
+- The student with index 1 scored 11 in exam 2, which is the highest score, so they got first place.
+- The student with index 0 scored 9 in exam 2, which is the second highest score, so they got second place.
+- The student with index 2 scored 3 in exam 2, which is the lowest score, so they got third place.
+Example 2:
+
+
+Input: score = [[3,4],[5,6]], k = 0
+Output: [[5,6],[3,4]]
+Explanation: In the above diagram, S denotes the student, while E denotes the exam.
+- The student with index 1 scored 5 in exam 0, which is the highest score, so they got first place.
+- The student with index 0 scored 3 in exam 0, which is the lowest score, so they got second place.
+ 
+
+Constraints:
+
+m == score.length
+n == score[i].length
+1 <= m, n <= 250
+1 <= score[i][j] <= 105
+score consists of distinct integers.
+0 <= k < n */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int m = sc.nextInt();
+        System.out.println("Enter number of cols: ");
+        int n = sc.nextInt();
+
+        int arr[][] = new int[m][n];
+        System.out.println("Enter "+m*n+" elements: ");
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++)arr[i][j] = sc.nextInt();
+        }
+        System.out.println("Enter k: ");
+        int k =sc.nextInt();
+
+        int ans[][] = sortTheStudents(arr, k);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++)System.out.print(ans[i][j]+" ");
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+    public static int[][] sortTheStudents(int[][] score, int k) {
+        Arrays.sort(score, (a,b)->b[k]-a[k]);
+        return score;
+    }
+}
