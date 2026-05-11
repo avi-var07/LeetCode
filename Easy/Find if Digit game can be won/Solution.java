c24e0b334d3f3f65567f07055b71ee0fdf9c9dd6
Aviral Varshney <aviralvarshney07@gmail.com>
2026-05-11 23:29:05 +0530

    Find if Digit game can be won

---
+++ Easy/Find if Digit game can be won/Solution.java
@@ -0,0 +1,71 @@
+/*You are given an array of positive integers nums.
+
+Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
+
+Return true if Alice can win this game, otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,10]
+
+Output: false
+
+Explanation:
+
+Alice cannot win by choosing either single-digit or double-digit numbers.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5,14]
+
+Output: true
+
+Explanation:
+
+Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+Example 3:
+
+Input: nums = [5,5,5,25]
+
+Output: true
+
+Explanation:
+
+Alice can win by choosing double-digit numbers which have a sum equal to 25.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 99 */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println(canAliceWin(arr));
+        sc.close();
+    }
+    public static boolean canAliceWin(int[] nums) {
+        int sum1=0;
+        int sum2=0;
+
+        for(int ele: nums){
+            if(ele<=9)sum1+=ele;
+            else sum2+=ele;
+        }
+
+        return !(sum1==sum2);
+    }
+}
