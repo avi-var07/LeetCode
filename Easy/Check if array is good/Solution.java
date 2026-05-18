62b98500efe75de1dc70021b479ab0b4f096751f
Aviral Varshney <aviralvarshney07@gmail.com>
2026-05-18 23:58:59 +0530

    Check if array is good

---
+++ Easy/Check if array is good/Solution.java
@@ -0,0 +1,60 @@
+/*You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+
+base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+
+Return true if the given array is good, otherwise return false.
+
+Note: A permutation of integers represents an arrangement of these numbers.
+
+ 
+
+Example 1:
+
+Input: nums = [2, 1, 3]
+Output: false
+Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. However, base[3] has four elements but array nums has three. Therefore, it can not be a permutation of base[3] = [1, 2, 3, 3]. So the answer is false.
+Example 2:
+
+Input: nums = [1, 3, 3, 2]
+Output: true
+Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. It can be seen that nums is a permutation of base[3] = [1, 2, 3, 3] (by swapping the second and fourth elements in nums, we reach base[3]). Therefore, the answer is true.
+Example 3:
+
+Input: nums = [1, 1]
+Output: true
+Explanation: Since the maximum element of the array is 1, the only candidate n for which this array could be a permutation of base[n], is n = 1. It can be seen that nums is a permutation of base[1] = [1, 1]. Therefore, the answer is true.
+Example 4:
+
+Input: nums = [3, 4, 4, 1, 2, 1]
+Output: false
+Explanation: Since the maximum element of the array is 4, the only candidate n for which this array could be a permutation of base[n], is n = 4. However, base[4] has five elements but array nums has six. Therefore, it can not be a permutation of base[4] = [1, 2, 3, 4, 4]. So the answer is false.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= num[i] <= 200 */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+        System.out.println(isGood(arr));
+        sc.close();
+    }
+    public static boolean isGood(int[] nums) {
+    
+        int n =nums.length;
+        Arrays.sort(nums);
+        if(nums[n-1]!=n-1||nums[n-2]!=n-1)return false;
+        for(int i=0;i<n-2;i++)if(nums[i]!=i+1)return false;
+        return true;
+    }
+}
