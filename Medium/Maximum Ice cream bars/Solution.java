eb44f95aea5237c0d8c545fcf1d64ea439f671da
Aviral Varshney <aviralvarshney07@gmail.com>
2026-06-21 23:10:27 +0530

    Maximum Ice Cream bars

---
+++ Medium/Maximum Ice cream bars/Solution.java
@@ -0,0 +1,66 @@
+/*It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+
+At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+
+Note: The boy can buy the ice cream bars in any order.
+
+Return the maximum number of ice cream bars the boy can buy with coins coins.
+
+You must solve the problem by counting sort.
+
+ 
+
+Example 1:
+
+Input: costs = [1,3,2,4,1], coins = 7
+Output: 4
+Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
+Example 2:
+
+Input: costs = [10,6,8,7,7,8], coins = 5
+Output: 0
+Explanation: The boy cannot afford any of the ice cream bars.
+Example 3:
+
+Input: costs = [1,6,3,1,2,5], coins = 20
+Output: 6
+Explanation: The boy can buy all the ice cream bars for a total price of 1 + 6 + 3 + 1 + 2 + 5 = 18.
+ 
+
+Constraints:
+
+costs.length == n
+1 <= n <= 105
+1 <= costs[i] <= 105
+1 <= coins <= 108
+ 
+ */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+        System.out.println("Enter coins: ");
+        int coins = sc.nextInt();
+
+        System.out.println(maxIceCream(arr, coins));
+        sc.close();
+    }
+    public static int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        long sum=0;
+        int ans=0;
+        for(int ele: costs){
+            sum+=ele;
+            if(sum<=coins)ans++;
+        }
+
+        return ans;
+    }
+}
